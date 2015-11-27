da846f4bef6d1141ae0cabd5b86b8c0915f934ec
Clement Auger <clement.ager@d1m.cn>
2015-11-27 10:06:57 +0800

    (example) Fix the README and added an example file to check the syntax

--- example.js
+++ example.js
@@ -1,29 +1,4 @@
-# it got limit
 
-A stream to limit rate of concurrent requests processed
-with `got` http module.
-
-## Install
-
-```sh
-    npm i maboiteaspam/it-got-limit
-```
-
-## API
-
-It s a stream transform made with `through2`.
-
-You input url, it produces got object at given rate.
-
-###### itGotLimit (limit, asStream)
-
-_limit_ : maximum number of simultaneous request.
-
-_asStream_ : receive data as a stream, or a callback (got.stream(url) || got(url).then(fn) )
-
-
-File: __example.js__
-```js
 var through2 = require('through2');
 var itGotLimit = require('it-got-limit');
 
@@ -63,9 +38,3 @@ stream.write('http://somewhere.com/')
 stream.write('http://somewhere.com/')
 
 stream.end()
-```
-
-## Read More
-
-- https://github.com/sindresorhus/got
-- https://github.com/rvagg/through2
